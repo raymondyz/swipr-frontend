cfdb378afadb2a2559f043d717a9b78d3ba32ffd
Raymond Zou <raymond.yh.zou@gmail.com>
2026-04-20 00:04:27 -0700

    Added regex validation for signup

--- src/utils/authValidation.js
+++ src/utils/authValidation.js
@@ -1,10 +1,15 @@
 
 export function validateSignupEmail(email) {
-  return true
+  const regex_email = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  const regex_ucla_email = /^[^\s@]+@(g\.)?ucla\.edu$/
+
+  return regex_email.test(email)
 }
 
 export function validateSignupPassword(password) {
-  return true
+  const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+
+  return regex.test(password)
 }
 
 export const passwordRequirements = "Password must be at least 8 characters and contain at least one letter and number"
